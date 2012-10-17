8ecd70c53ee1f85503a9789946ac057079b1fe12
dvdrtrgn <dvdrtrgn@gmail.com>
2012-10-17 00:56:17 -0500

    direction reaction

--- ex4/etc.js
+++ ex4/etc.js
@@ -4,31 +4,48 @@ $(function(){
     ,   field = $('#field')
     ,   trans = -500
     ,   c0 = $('#c0')
+    ,   arrows = $('.arrow').css('z-index', 9999)
     ;
     faces.click(function (){
         field.toggleClass('flip');
     });
     field.mousewheel(function (event, delta, deltaX, deltaY) {
-        var o = '';
+        var o = ''
+        ,   p = ''
+        ;
 
-        if (deltaX > 0) {
-            o += 'E∆ ' + deltaX;
-        } else if (deltaX < 0) {
-            o += 'W∆ ' + -deltaX;
-        }
         if (deltaY > 0) {
-            o += 'N∆ ' + deltaY;
+            o += 'N';
+            p += deltaY;
         } else if (deltaY < 0){
-            o += 'S∆ ' + -deltaY;
+            o += 'S';
+            p += -deltaY;
+        }
+        if (deltaX > 0) {
+            o += 'E';
+            p += deltaX;
+        } else if (deltaX < 0) {
+            o += 'W';
+            p += -deltaX;
         }
         event.preventDefault();
-        console.log(o, event, delta, deltaX, deltaY);
+        //console.log(o, p, event, delta, deltaX, deltaY);
         transX(delta);
+        cursor(o, event);
     });
     console.log('o, event, delta, deltaX, deltaY');
     function transX(diff){
         trans += parseFloat(diff || 0) * 10;
         c0.css('-webkit-transform','translateZ('+ trans +'px)');
+    }
+    function cursor(str, evt){
+        str = str.toLowerCase();
+        field.css('cursor', str + '-resize');
+        arrows.removeClass().addClass('_' + str);
+        arrows.css({
+            left: evt.clientX,
+            top: evt.clientY
+        });
     }
 });
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
