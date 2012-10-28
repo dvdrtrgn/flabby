f089df401e82ad76eddc7cbd53ebac08c89c9fc8
dvdrtrgn <dvdrtrgn@gmail.com>
2012-10-28 14:46:45 -0500

    per experiment readmes

--- ex5/etc.js
+++ ex5/etc.js
@@ -1,3 +1,16 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+function defineSizes() {
+    $('body').css({
+        width: window.innerWidth / 3 + 'px',
+        height: window.innerHeight / 3 + 'px',
+    });
+}
+
+function init() {
+    $(window).bind('resize load', defineSizes);
+}
+
+//$(init);
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
