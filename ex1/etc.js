6370eff4be7f1d5231615d833dd45c860af76fda
dvdrtrgn <dvdrtrgn@gmail.com>
2012-11-10 00:27:56 -0600

    refine ex1

--- ex1/etc.js
+++ ex1/etc.js
@@ -1,28 +1,35 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 var tmp;
 
-function fakeAccessor(_n) {
+function accessor(pth, _n) {
+    var self;
     return function (n) {
+        self = self || resolve(this, pth);
         if (n) {
-            this.box.style[_n] = n + 'px';
+            self[_n] = n + 'px';
         } else {
-            return parseInt(this.box.style[_n]) || 0;
+            return parseInt(self[_n]) || 0;
         }
     };
 }
 
+function resolve(obj, str) {
+    var own = obj,
+        pth = str.split('.');
+    while (pth.length) own = own[pth.shift()];
+    console.log('resolve', str, '@', obj, '...', own);
+    return own;
+}
+
 function Box(cf) {
-    var x, y, w, h // primaries
-    ,   box = $('<div class="box">').appendTo('body').get(0)
-    ;
-    this.box = box;
+    this.div = $('<div class="box">').appendTo('body').get(0);
     cf && this.nport(cf);
 }
 $.extend(Box.prototype, {
-    x: fakeAccessor('left'),
-    y: fakeAccessor('top'),
-    w: fakeAccessor('width'),
-    h: fakeAccessor('height'),
+    x: accessor('div.style', 'left'),
+    y: accessor('div.style', 'top'),
+    w: accessor('div.style', 'width'),
+    h: accessor('div.style', 'height'),
     nport: function (o) {
         (o.left !== undefined) && this.x(o.left);
         (o.top !== undefined) && this.y(o.top);
@@ -37,9 +44,26 @@ $.extend(Box.prototype, {
             height: this.h(),
         };
     },
-    centerAt: function (x0, y0) {
+    // take div to quarters by adding add two divs inside with borders
+    xhair: function () {
+        var q0 = $(this.div)
+        ,   q1 = $('<div class="q1">') // top right
+        ,   q3 = $('<div class="q3">') // bottom left
+        ;
+        q0.append(q1, q3);
+        return this;
+    },
+    xhere: function (x0, y0) {
         this.x((x0 || 0) - this.w() / 2);
         this.y((y0 || 0) - this.h() / 2);
+        return this;
+    },
+    trace: function () {
+        var self = this.xhair();
+        $('html').bind('mouseup', function (e) {
+            self.xhere(e.pageX, e.pageY);
+        });
+        return this;
     }
 });
 
@@ -49,11 +73,7 @@ function init() {
         top: 33,
         width: 166,
         height: 99
-    });
-    $('html').bind('mouseup', function (e) {
-        tmp.centerAt(e.pageX, e.pageY);
-    });
-    console.log(tmp);
+    }).trace();
 }
 
 $(init);
