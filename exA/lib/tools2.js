e608d8fe4d0dfdb6093b1f8c5f93e653186365b9
dvdrtrgn <dvdrtrgn@gmail.com>
2012-12-25 12:51:33 -0600

    document

--- exA/lib/tools2.js
+++ exA/lib/tools2.js
@@ -13,10 +13,10 @@ function columnSort(tab, key, def) { //         def = default (for undefined)
 function keyObject(dat) { //    keys of every record/row in source
     var k, obj = {};
     dat.forEach(function (row) {
-        var num = 1; //                         begin property count
+        var num = 1; //                     begin property count
         $.each(row, function (k, v) {
             obj[k] = obj[k] || [];
-            obj[k].push(num++); //              store order of k in property
+            obj[k].push(num++); //          store order of k in property
         });
     });
     $.each(obj, function (i, e) {
@@ -33,13 +33,13 @@ function obj2arrs(obj) { //             take poly and boolean
 }
 
 function rundef(v, alt) {
-    return hasdef(v) ? v : alt; //              redefine undefined
+    return hasdef(v) ? v : alt; //          redefine undefined
 }
 
-function transpose(dat) { //                    take 2-D array to flip diagonally
+function transpose(dat) { //                take 2-D array to flip diagonally
     var tab = [];
-    dat[0].forEach(function (z, col) { //       for each column (in first row)
-        tab.push(column(dat, col)); //          append row of columns
+    dat[0].forEach(function (z, col) { //   for each column (in first row)
+        tab.push(column(dat, col)); //      append row of columns
     });
     return tab;
 }
