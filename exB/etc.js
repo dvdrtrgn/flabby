3639634571e64210f48177b06e77a2be91f354b3
dvdrtrgn <dvdrtrgn@gmail.com>
2012-12-27 09:08:05 -0600

    exB webstorage

---
+++ exB/etc.js
@@ -0,0 +1,14 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+sessionStorage.setItem('keyA', 'Aval');
+var a = sessionStorage.getItem('keyA');
+
+localStorage.setItem('keyB', 'Bval');
+var b = localStorage.getItem('keyB');
+
+window.addEventListener('storage', function (event) {
+    console.log('The value for ' + event.key //
+    + ' was changed from ' + event.oldValue //
+    + ' to ' + event.newValue);
+}, false);
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
