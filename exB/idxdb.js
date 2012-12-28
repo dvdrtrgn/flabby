fbd0dcf110e4bcb7b8271e3b7c10759a69f15b77
dvdrtrgn <dvdrtrgn@gmail.com>
2012-12-28 02:12:51 -0600

    exB abstract todos

--- exB/idxdb.js
+++ exB/idxdb.js
@@ -6,90 +6,103 @@
     window.IDBTransaction = (window.IDBTransaction || window.webkitIDBTransaction);
     window.IDBKeyRange = (window.IDBKeyRange || window.webkitIDBKeyRange);
 
-    var DB = $.DB = { // namespace
-        // db :: holds the real instance of the indexedDB
-        db: null,
+    $.Plugin = function () {}
+
+    var DB = $.DB = $.extend(new $.Plugin, {
         //    METHODS
-        /* open (create) */
         err: function () {
             console.error(arguments);
         },
-        open: function () {
-            var req = indexedDB.open('todos', 5);
+        /* make (create) */
+        make: function (key) {
+            DB.key = key;
+            key += 's'; // make "plural"
+            var req = indexedDB.open(key, 5);
             clog(req);
-            req.onupgradeneeded = DB.err;
-            req.onsuccess = function (e) {
-                // clog('onsuccess', e);
-                DB.db = e.target.result;
-                var db = DB.db;
-                // clog('db', db);
-                DB.getAllTodoItems();
+            req.onsuccess = function (evt) {
+                DB[DB.key] = evt.target.result;
+                clog('onsuccess', evt, 'result :: DB[key]', DB[DB.key]);
+                DB.getAll();
             };
+            req.onupgradeneeded = DB.err;
         },
-        /* addTodo */
-        addTodo: function (str) {
-            var db = DB.db,
-                trans = db.transaction(['todo'], 'readwrite'),
-                store = trans.objectStore('todo'),
-                req;
-            req = store.put({
+        /* todo Add */
+        todo_Add: function (str) {
+            return {
                 'text': str,
                 'timeStamp': ($.now() - 1356672670000),
-            })
+            };
+        },
+        /* todo GetAll */
+        todo_GetAll: function () {
+            $('#todos').empty();
+            return function (x) {
+                new Todo(x.value);
+            }
+        },
+        /* add */
+        add: function (str) {
+            var store = DB.getStore(),
+                req = DB.todo_Add(str);
+            req = store.put(req)
             req.onsuccess = function (e) {
-                DB.getAllTodoItems(); // Re-render all the todo's
+                DB.getAll(); // re-render all
             };
             req.onerror = DB.err;
         },
-        /* getAllTodoItems (read) */
-        getAllTodoItems: function () {
-            $('#todos').empty();
-
-            var db = DB.db,
-                trans = db.transaction(['todo'], 'readwrite'),
-                store = trans.objectStore('todo'),
+        /* getAll (read) */
+        getAll: function () {
+            var store = DB.getStore(),
                 keyRange = IDBKeyRange.lowerBound(0),
-                cursorRequest = store.openCursor(keyRange);
-            //
-            cursorRequest.onsuccess = function (e) {
+                rex = store.openCursor(keyRange),
+                cb = DB.todo_GetAll();
+            rex.onsuccess = function (e) {
                 var result = e.target.result;
                 if (result) {
-                    new Todo(result.value);
+                    cb(result);
                     result['continue']();
                 }
             };
-            cursorRequest.onerror = DB.err;
+            rex.onerror = DB.err;
         },
-        /* deleteTodo */
-        deleteTodo: function (id) {
-            var DB = this,
-                db = DB.db,
-                trans = db.transaction(['todo'], 'readwrite'),
-                store = trans.objectStore('todo'),
+        /* remove */
+        remove: function (id) {
+            var store = DB.getStore(),
                 req = store.delete(id);
-            //
             req.onsuccess = function (e) {
-                DB.getAllTodoItems(); //      Refresh the screen
+                DB.getAll(); //      Refresh the screen
             };
             req.onerror = DB.err;
         },
+        getStore: function () {
+            return DB[DB.key] //
+            .transaction([DB.key], 'readwrite') //
+            .objectStore(DB.key);
+        },
+        todo_Init: function () {
+            var key = 'todo',
+                txt = $('#' + key);
+            div = $('#' + key + 's');
+            $('[type=submit]').on('click', function (e) {
+                DB.add(txt.val());
+                txt.val('');
+                return false;
+            });
+            div.on('click', '.remove', function (e) {
+                DB.remove(parseInt($(this).parent().find('.id').text()));
+                return false;
+            });
+            DB.make(key); // make displays the data previously saved
+        },
         init: function () {
             clog('DB', DB);
-            $('[type=submit]').on('click', function (e) {
-                DB.addTodo($('#todo').val());
-                $('#todo').val('');
-                return false;
-            });
-            $('#todos').on('click', '.delete', function (e) {
-                DB.deleteTodo(parseInt($(this).parent().find('.id').text()));
-                return false;
-            });
-            DB.open(); // open displays the data previously saved
+            DB.todo_Init();
         },
-    };
+    });
 })(jQuery);
 
 function Todo(row) {
+    this.div = $('#todos');
     this.row = row;
     this.renderTodo();
 }
@@ -98,10 +111,10 @@ $.extend(Todo.prototype, {
     renderTodo: function () {
         var row = this.row,
             li = $('<li>') //
-        .append('<a href="#" class="delete">delete</a>') //
+        .append('<a href="#" class="remove">remove</a>') //
         .append(row.text) //
         .append('<span class="id">' + row.timeStamp + '</span>') //
-        .appendTo(('#todos'));
+        .appendTo(this.div);
     },
 })
 
