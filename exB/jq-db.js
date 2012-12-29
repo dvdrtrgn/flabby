a4b641c7391e490d7f7636f874a24f4724130120
dvdrtrgn <dvdrtrgn@gmail.com>
2012-12-29 00:42:13 -0600

    refact

--- exB/jq-db.js
+++ exB/jq-db.js
@@ -101,26 +101,4 @@
     });
 })(jQuery);
 
-function Todo(row) {
-    this.div = $('#todos');
-    this.row = row;
-    this.renderTodo();
-}
-
-$.extend(Todo.prototype, {
-    renderTodo: function () {
-        var row = this.row,
-            li = $('<li>') //
-        .append('<a href="#" class="remove">remove</a>') //
-        .append(row.text) //
-        .append('<span class="id">' + row.timeStamp + '</span>') //
-        .appendTo(this.div);
-    },
-})
-
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-/* init / events */
-$(function () {
-    $.DB.init();
-});
